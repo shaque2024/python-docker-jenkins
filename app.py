2bc7c254bdf2bad08445a934104e33f58f0ab195
shaque2024 <sazzadulhaque996@gmail.com>
2025-09-04 17:51:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return "<h1>Welcome to Jenkins Tutorials<h1/>"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0'
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0' , port = port)
+
